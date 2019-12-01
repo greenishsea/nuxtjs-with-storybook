331c4abab7e0040d5277ee2077c3216e9150ceb8
hiroyuki <hiroyuki@mbp15.local>
2019-12-02 01:09:32 +0900

    Use addParameters instead of addon-options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,17 @@
-import { configure, addDecorator } from '@storybook/vue';
-import { withOptions } from '@storybook/addon-options';
-import Centered from '@storybook/addon-centered/vue';
+import { configure, addParameters, addDecorator } from '@storybook/vue'
+import Centered from '@storybook/addon-centered/vue'
 
-addDecorator(
-  withOptions({
-      name: 'Storybook for Nuxt Project',
-      hierarchyRootSeparator: /\|/,
-      // url: '',
-      // selectedAddonPanel: 'storybook/stories/stories-panel' // obsolete property ?
-      // selectedPanel: 'storybook/actions/panel' // new one
-  })
-)
+addParameters({ options: {
+  name: 'Storybook for Nuxt Project',
+  hierarchyRootSeparator: /\|/,
+  // url: '',
+  // selectedAddonPanel: 'storybook/stories/stories-panel' // obsolete property ?
+  // selectedPanel: 'storybook/actions/panel' // new one
+}})
+
 addDecorator(Centered)
 // addDecorator(() => ({
 //   template: '<div style="textAlign: center"><story/></div>',
 // }));
 
-configure(require.context('../components', true, /\.stories\.(js|mdx)$/), module);
+configure(require.context('../components', true, /\.stories\.(js|mdx)$/), module)
